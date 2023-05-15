227799dfb483ea5a8c358858a90ada493fc049cd
Herbert Cuyugan <herbert.cuyugan@pentanasolutions.com>
2023-05-16 00:10:30 +1000

    more of the error handling practise

--- main.py
+++ main.py
@@ -73,17 +73,22 @@ def save():
         #                                               f"\nPassword: {password} \nIs it ok to save?")
 
         # if is_ok:
-        with open("data.json", "r") as data_file:
+        try:
+            with open("data.json", "r") as data_file:
             # Reading Old data
             # json.dump(new_data, data_file, indent=4)
-            data = json.load(data_file)
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             # updating old data with new data
             data.upload(new_data)
-
-        with open("data.json", "w") as data_file:
-            # Saving updated data
-            json.dump(data, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
